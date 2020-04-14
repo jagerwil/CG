5287960031e7aa36645232da5eefd952808cc8b2
jagerwil <vlad1.0@list.ru>
2020-04-14 16:27:21 +0300

    Added pr8

---
+++ Pr8/Assets/Scripts/SphereAnimController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereAnimController : MonoBehaviour
+{
+    private Animator _animator;
+    
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("[E]: Animation changed!");
+            _animator.SetTrigger("SwitchAnimTrigger");
+        }
+    }
+}
