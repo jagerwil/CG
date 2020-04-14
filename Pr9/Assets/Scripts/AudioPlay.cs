7445039c6877923a32f098118ddab7c3bd14d2f3
jagerwil <vlad1.0@list.ru>
2020-04-14 19:51:37 +0300

    Added Pr9

---
+++ Pr9/Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    private AudioSource _audio;
+
+
+    void Start()
+    {
+        _audio = transform.Find("Audio Source").GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _audio.Play();
+    }
+}
