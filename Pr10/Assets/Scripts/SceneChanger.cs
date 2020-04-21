2672007fee08b5ea6af370a45d0953af049ce6c2
jagerwil <vlad1.0@list.ru>
2020-04-21 23:56:05 +0300

    Added Pr10

---
+++ Pr10/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E pressed!");
+            SceneManager.LoadScene("SteamScene");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("R pressed!");
+            SceneManager.LoadScene("SnowScene");
+        }
+    }
+}
