25e662e95dde6a40d55dc7e8384303575d093aed
jagerwil <vlad1.0@list.ru>
2020-04-28 21:53:02 +0300

    Added Pr11

---
+++ Pr11/Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private Image image;
+    [SerializeField] private Sprite changeSprite;
+
+
+    public void Close()
+    {
+        panel.SetActive(false);
+    }
+
+    public void ChangeImage()
+    {
+        image.sprite = changeSprite;
+    }
+}
