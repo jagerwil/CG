9b827b92b82728b44c7c9f1ee6b2c4f2220df9c6
jagerwil <vlad1.0@list.ru>
2020-05-01 22:34:32 +0300

    Added Pr12

---
+++ Pr12/Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void Options(GameObject window)
+    {
+        window.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
