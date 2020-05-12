11f660f2aca088daa5cb3d32513cbffb780d4ade
jagerwil <vlad1.0@list.ru>
2020-05-12 20:39:42 +0300

    Added Pr13

---
+++ Pr13/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float _mouseSensitivity = 100f;
+    [SerializeField] private Transform _controller;
+    private float _rotationX;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;   
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        _rotationX -= mouseY;
+        _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
+        _controller.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Pr13/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _jumpHeight = 1f;
+    [SerializeField] private float _gravity = -9.8f;
+    [SerializeField] private Transform _checkGround;
+    [SerializeField] private float _checkGroundRadius = 0.4f;
+    [SerializeField] private LayerMask _groundLayer;
+
+    private CharacterController _charController;
+    private Vector3 _velocity = Vector3.zero;
+    private bool _isGrounded = false;
+
+    private void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        movement = movement.normalized * _speed;
+
+        _isGrounded = Physics.CheckSphere(_checkGround.position, _checkGroundRadius, _groundLayer);
+        movement.y = _isGrounded ? 0f : _velocity.y + _gravity * Time.deltaTime;
+        _velocity = transform.rotation * movement;
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * _gravity * -2);
+        }
+        _charController.Move(_velocity * Time.deltaTime);
+    }
+}
