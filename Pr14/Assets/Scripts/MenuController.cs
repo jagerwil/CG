61358c41f8ab4194d61be57d9b40a8bf3d7cc402
jagerwil <vlad1.0@list.ru>
2020-05-28 01:49:41 +0300

    Added Pr14

---
+++ Pr14/Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject _spherePrefab;
+    [SerializeField] private GameObject _cubePrefab;
+    [SerializeField] private GameObject _menuObject;
+    [SerializeField] private Transform _spawnPoint;
+
+    private bool _isActive;
+    
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            _isActive = !_isActive;
+            _menuObject.SetActive(_isActive);
+        }
+    }
+
+    public void SpawnSphere()
+    {
+        Instantiate(_spherePrefab, _spawnPoint.position, _spawnPoint.rotation);
+    }
+
+    public void SpawnCube()
+    {
+        Instantiate(_cubePrefab, _spawnPoint.position, _spawnPoint.rotation);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
